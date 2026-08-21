09ebd010baa893005fd052a1051bbb52cfb2f7bd
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-21 14:38:59 +0300

    Add attachment controller to message controller

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,7 +1,20 @@
-import { createMessage, getConversationMessages } from '../services/message.service.js';
+import {
+  createAttachmentMessage,
+  createMessage,
+  getConversationMessages,
+} from '../services/message.service.js';
 
 export async function createMessageController(req, res) {
   const message = await createMessage(req.params.conversationId, req.user.id, req.body);
+
+  res.status(201).json({
+    success: true,
+    message,
+  });
+}
+
+export async function createAttachmentMessageController(req, res) {
+  const message = await createAttachmentMessage(req.params.conversationId, req.user.id, req.file);
 
   res.status(201).json({
     success: true,
@@ -13,7 +26,7 @@ export async function getConversationMessagesController(req, res) {
   const messages = await getConversationMessages(
     req.params.conversationId,
     req.user.id,
-    res.locals.query.limit,
+    req.query.limit,
   );
 
   res.status(200).json({
