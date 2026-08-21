d515acb2ef637a19711eb836d83f8f34df47bb07
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-21 13:15:30 +0300

    Use socket for server

--- src/server.js
+++ src/server.js
@@ -2,8 +2,11 @@ import http from 'node:http';
 
 import app from './app.js';
 import { env } from './config/env.js';
+import { createSocketServer } from './sockets/index.js';
 
 const server = http.createServer(app);
+
+createSocketServer(server);
 
 server.listen(env.PORT, () => {
   console.log(`Server running on http://localhost:${env.PORT}`);
