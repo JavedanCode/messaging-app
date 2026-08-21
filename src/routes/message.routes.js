13d7e93ceea3c2a35bf8b93987e03c8e24e17a14
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-21 12:56:34 +0300

    Use pagination in message routes

--- src/routes/message.routes.js
+++ src/routes/message.routes.js
@@ -6,7 +6,7 @@ import {
 } from '../controllers/message.controller.js';
 
 import { authenticate } from '../middleware/authenticate.js';
-import { validate, validateParams } from '../middleware/validate.js';
+import { validate, validateParams, validateQuery } from '../middleware/validate.js';
 
 import { conversationIdParamsSchema } from '../schemas/conversation.schema.js';
 
@@ -26,6 +26,7 @@ router.post(
 router.get(
   '/:conversationId/messages',
   validateParams(conversationIdParamsSchema),
+  validateQuery(messagePaginationSchema),
   getConversationMessagesController,
 );
 
