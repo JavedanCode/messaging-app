722b82a0be4a17d2ed2c77700551deb803870161
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-21 12:31:31 +0300

    Mount message router to app

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ import { configurePassport } from './config/passport.js';
 import authRoutes from './routes/auth.routes.js';
 import userRouter from './routes/user.routes.js';
 import conversationRoutes from './routes/conversation.routes.js';
+import messageRoutes from './routes/message.routes.js';
 
 import { errorHandler } from './middleware/error-handler.js';
 
@@ -42,6 +43,7 @@ app.use(passport.initialize());
 app.use('/auth', authRoutes);
 app.use('/users', userRouter);
 app.use('/conversations', conversationRoutes);
+app.use('/conversations', messageRoutes);
 
 app.get('/health', (req, res) => {
   res.status(200).json({
