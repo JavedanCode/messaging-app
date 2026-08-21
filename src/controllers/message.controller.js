4d69eeb2529d9de7beaa4ab96a13e377aa57cf3e
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-21 12:30:40 +0300

    Build base message controller

---
+++ src/controllers/message.controller.js
@@ -0,0 +1,23 @@
+import { createMessage, getConversationMessages } from '../services/message.service.js';
+
+export async function createMessageController(req, res) {
+  const message = await createMessage(req.params.conversationId, req.user.id, req.body);
+
+  res.status(201).json({
+    success: true,
+    message,
+  });
+}
+
+export async function getConversationMessagesController(req, res) {
+  const messages = await getConversationMessages(
+    req.params.conversationId,
+    req.user.id,
+    req.query.limit,
+  );
+
+  res.status(200).json({
+    success: true,
+    messages,
+  });
+}
