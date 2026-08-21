c3ef82b0c839723ad3ab0d4f776630f3d47e1b29
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-21 15:24:18 +0300

    Add cloudflare bucket info to env

--- src/config/env.js
+++ src/config/env.js
@@ -37,6 +37,11 @@ const envSchema = z.object({
 
   PASSWORD_RESET_URL: z.url(),
   EMAIL_CHANGE_URL: z.url(),
+
+  R2_ACCOUNT_ID: z.string().min(1),
+  R2_ACCESS_KEY_ID: z.string().min(1),
+  R2_SECRET_ACCESS_KEY: z.string().min(1),
+  R2_BUCKET_NAME: z.string().min(1),
 });
 
 const result = envSchema.safeParse(process.env);
