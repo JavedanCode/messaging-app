b436a7654b5dbeb673b249d79f387b626093e05d
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-21 13:03:45 +0300

    Use validateQuery in routes

--- src/routes/message.routes.js
+++ src/routes/message.routes.js
@@ -6,6 +6,7 @@ import {
 } from '../controllers/message.controller.js';
 
 import { authenticate } from '../middleware/authenticate.js';
+
 import { validate, validateParams, validateQuery } from '../middleware/validate.js';
 
 import { conversationIdParamsSchema } from '../schemas/conversation.schema.js';
