019dc31b8c51985c715744b460c46901cd2f21fe
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-22 12:46:26 +0300

    Add deletion emitter for messages

--- src/sockets/message.socket.js
+++ src/sockets/message.socket.js
@@ -23,3 +23,16 @@ export function emitMessageUpdated(message) {
 
   io.to(getConversationRoom(message.conversationId)).emit('message:updated', message);
 }
+
+export function emitMessageDeleted(conversationId, messageId) {
+  const io = getSocketServer();
+
+  if (!io) {
+    return;
+  }
+
+  io.to(getConversationRoom(conversationId)).emit('message:deleted', {
+    messageId,
+    conversationId,
+  });
+}
