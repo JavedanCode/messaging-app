26387e2e96d678a9ddf7bf1d649a88f0b4b7deb0
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-24 17:49:23 +0300

    Configure client providers

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import "./index.css";
 import App from "./App.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { ChatProvider } from "./context/ChatContext.jsx";
+import { FriendshipProvider } from "./context/FriendshipContext.jsx";
+import { SocketProvider } from "./context/SocketContext.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <SocketProvider>
+          <FriendshipProvider>
+            <ChatProvider>
+              <App />
+            </ChatProvider>
+          </FriendshipProvider>
+        </SocketProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>,
 );
