126f2715f99fdadb7d9622fdd1965c1320cb4fb6
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-24 19:52:00 +0300

    fix Github pages deployment path

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,7 +11,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/messaging-app">
       <AuthProvider>
         <SocketProvider>
           <FriendshipProvider>
